547473b504170c6be178f424a4ff47ea5162c729
hector3211 <hectororopesa5@gmail.com>
2022-11-13 23:45:41 -0500

    numSum and subsequence addition to day ten

--- go/day10/main.go
+++ go/day10/main.go
@@ -1,12 +1,66 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
-type User struct {
-	Name string
-	Tag  int
+func YoOne(arr []int, target int) []int {
+	hashy := make(map[int]int)
+	for _, v := range arr {
+		potential := target - v
+		if _, ok := hashy[potential]; ok {
+			return []int{potential, v}
+		}
+		hashy[v] = target - v
+	}
+	return []int{}
+}
+
+func YoTwo(arr []int, seq []int) bool {
+	index := 0
+	for _, v := range arr {
+		if v == seq[index] {
+			index++
+		}
+		if len(seq) == index {
+			return true
+		}
+	}
+	return false
+}
+
+func YoThree(arr []int) []int {
+	startP := 0
+	endP := len(arr) - 1
+	sqList := make([]int, len(arr))
+    for i := range arr  {
+		start := math.Floor(math.Sqrt(float64(arr[startP])))
+		end := math.Floor(math.Sqrt(float64(arr[endP])))
+		if end > start {
+			sqList[i] = int(end)
+			end--
+		} else {
+			sqList[i] = int(start)
+            start++
+		}
+	}
+	return sqList
 }
 
 func main() {
-	fmt.Println("Hello")
+	// two num sum
+	array := []int{3, 5, -4, 8, 11, 1, -1, 6}
+	targetNum := 8
+	fmt.Println(YoOne(array, targetNum))
+
+	// validate sequence
+	arrayTwo := []int{5, 1, 22, 25, 6, -1, 8, 10}
+	sequence := []int{1, 6, -1, 10}
+	fmt.Println(YoTwo(arrayTwo, sequence))
+
+	// sqroot sorted array
+	arrayThree := []int{1, 2, 3, 5, 6, 8, 9}
+	fmt.Println(YoThree(arrayThree))
+
 }
--- go/day9/main.go
+++ go/day9/main.go
@@ -2,43 +2,41 @@ package main
 
 import (
 	"fmt"
-	"testing"
 )
 
-func main () {
-    fmt.Println("hellow")
-    array := []int{5,1,2,3}
-    fmt.Println(Yoo(array))
-                fmt.Println()
-    fmt.Println(Yo2(array))
+func main() {
+	fmt.Println("hellow")
+	array := []int{5, 1, 2, 3}
+	fmt.Println(Yoo(array))
+	fmt.Println()
+	fmt.Println(Yo2(array))
 }
 
-type MyType interface{
-    ~int | ~string
-}
-func Yoo[T MyType](arr []T) []T{
-    var list []T
-    for _,n := range arr{
-        list = append(list,n + T(1))
-    }
-    return list
+type MyType interface {
+	~int | ~string
 }
 
-func Yo2[T MyType](arr []T) []T{
-    list := make([]T,len(arr))
-    const one int = 1
-    for i,v := range arr{
-        list[i] = v + T(1)
-    }
-    return list
+func Yoo[T MyType](arr []T) []T {
+	var list []T
+	for _, n := range arr {
+		list = append(list, n+T(1))
+	}
+	return list
+}
+
+func Yo2[T MyType](arr []T) []T {
+	list := make([]T, len(arr))
+	const one int = 1
+	for i, v := range arr {
+		list[i] = v + T(1)
+	}
+	return list
 }
 
 func Yo3(arr []int) []int {
-    list := make([]int, len(arr))
-    for i,v := range arr{
-                    list[i] = v + 1
-    }
-    return list
+	list := make([]int, len(arr))
+	for i, v := range arr {
+		list[i] = v + 1
+	}
+	return list
 }
-
-
