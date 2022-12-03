c4a788414bc0152b636d075a9eeab19b188ef4dd
hector3211 <hectororopesa5@gmail.com>
2022-12-03 12:22:18 -0500

    added remove method to doublelinkedlits.ts

--- doublelinkedlist.ts
+++ doublelinkedlist.ts
@@ -37,6 +37,35 @@ class Dlist<T> {
     this.tail = newNode;
   }
 
+  remove(item: T): void {
+    if (this.length === 0) {
+      return;
+    }
+    let curr = this.head;
+    while (curr) {
+      if (curr.value === item) {
+        break;
+      }
+      curr = curr.next;
+    }
+    if (!curr) {
+      return;
+    }
+    this.length--;
+    if (curr.prev) {
+      curr.prev.next = curr.next;
+    }
+    if (curr.next) {
+      curr.next.prev = curr.prev;
+    }
+    if (curr === this.head) {
+      this.head = curr.next;
+    }
+    if (curr === this.tail) {
+      this.tail = curr.prev;
+    }
+  }
+
   getall(): void {
     let curr = this.head;
     if (!curr) {
@@ -52,4 +81,9 @@ class Dlist<T> {
 const dbl = new Dlist();
 dbl.preappend(19);
 dbl.append(29);
+dbl.append(39);
+dbl.append(59);
+dbl.getall();
+dbl.remove(59);
+console.log("removing.....");
 dbl.getall();
