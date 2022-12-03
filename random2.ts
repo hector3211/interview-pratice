c6045771b0d82eeb8c5022a778defe528413e7c7
hector3211 <hectororopesa5@gmail.com>
2022-12-03 00:26:09 -0500

    [added]: eslint,typescript plugins and finished doublelinkedlist in typescript

--- random2.ts
+++ random2.ts
@@ -1,32 +1,41 @@
+// this is double linked list not a binary tree
 interface BNode<T> {
-  value: T;
-  prev: BNode<T>;
-  next: BNode<T>;
+    value: T;
+    prev: BNode<T>;
+    next: BNode<T>;
 }
 
 class BsTree<T> {
-  length: T;
-  value: T;
-  head: BNode<T>;
-  tail: BNode<T>;
-  constructor() {
-    this.length;
-    this.value;
-    this.head = undefined;
-    this.tail = undefined;
-  }
+    length?: T;
+    head?: BNode<T>;
+    tail?: BNode<T>;
+    constructor() {
+        this.head = undefined;
+        this.tail = undefined;
+    }
+
+    prepend(item: T): void {
+        const newNode = { value: item } as BNode<T>;
+        if (!this.head) {
+            this.head = this.tail = newNode;
+        }
+        newNode.next = this.head;
+        this.head.prev = newNode;
+        this.head = newNode;
+    }
+
+    append(item: T): void {
+        const newNode = { value: item } as BNode<T>;
+        if (!this.tail) {
+            this.tail = this.head = newNode;
+        }
+        newNode.prev = this.tail;
+        this.tail.next = newNode;
+        this.tail = newNode;
+    }
 }
 
-function Prepend<T>(item: T, tree: BsTree<T>): void {
-  const newNode = { value: item } as BNode<T>;
-  if (!tree.head) {
-    this.head = this.tail = newNode;
-  }
-  newNode.next = tree.head;
-  tree.head.prev = newNode;
-  tree.head = newNode;
-}
-
-const bst = new BsTree();
-Prepend(10, bst as BsTree<number>);
-console.log(bst);
+const tre = new BsTree();
+tre.prepend(10);
+tre.append(12);
+console.log(tre);
