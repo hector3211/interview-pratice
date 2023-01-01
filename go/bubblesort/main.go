2aeb9c06ebc98eddfefc86a27d9f5e48c10c70e8
hector3211 <hectororopesa5@gmail.com>
2023-01-01 14:22:21 -0500

    changed: go/bubblesort

--- go/bubblesort/main.go
+++ go/bubblesort/main.go
@@ -4,9 +4,9 @@ import "fmt"
 
 func BubbleSort(arr []int) []int {
 	for i := 0; i < len(arr); i++ {
-		for j := 0; j < len(arr)-1-i; j++ {
-			if arr[j] > arr[j+1] {
-				arr[j], arr[j+1] = arr[j+1], arr[j]
+		for j := i + 1; j < len(arr); j++ {
+			if arr[i] > arr[j] {
+				arr[i], arr[j] = arr[j], arr[i]
 			}
 		}
 	}
